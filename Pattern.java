46aad67c2bf2d6e3f878f929faa541553884dc22
M bharath <60010887+medurubharath@users.noreply.github.com>
2023-01-05 23:11:48 +0530

    Update Pattern.java

--- Pattern.java
+++ Pattern.java
@@ -50,3 +50,23 @@ public class YourClassNameHere {
     }
 }
 
+*
+ *
+  *
+  public class YourClassNameHere {
+    public static void main(String[] args) {
+      for(int i=1;i<=3;i++)
+      {
+        for(int j=1;j<=3;j++)
+        {
+          if(i==j)
+            System.out.print("*");
+          else
+          System.out.print(" ");
+        }
+        System.out.println();
+      }
+    }
+}
+
+
