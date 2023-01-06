e9bb4dbce76a68d5ee60f7488cd7cfc13fe83ba4
M bharath <60010887+medurubharath@users.noreply.github.com>
2023-01-06 15:45:51 +0530

    Update Pattern.java

--- Pattern.java
+++ Pattern.java
@@ -70,3 +70,62 @@ public class YourClassNameHere {
 }
 
 
+*
+**
+***
+****
+*****
+public class YourClassNameHere {
+    public static void main(String[] args) {
+      for(int i=1;i<=5;i++)
+      {
+        for(int j=1;j<=i;j++)
+        { 
+            System.out.print("*");
+        }
+        System.out.println();
+      }
+    }
+}
+
+   * 
+  *  
+ *   
+*
+public class YourClassNameHere {
+    public static void main(String[] args) {
+      for(int i=1;i<=5;i++)
+      {
+        for(int j=1;j<=5;j++)
+        { 
+          if(i+j==5)
+            System.out.print("*");
+          else
+            System.out.print(" ");
+        }
+        System.out.println();
+      }
+    }
+}
+
+
+    *
+   **
+  ***
+ ****
+*****
+public class YourClassNameHere {
+    public static void main(String[] args) {
+      for(int i=1;i<=5;i++)
+      {
+        for(int j=1;j<=5;j++)
+        { 
+          if(i+j>5)
+            System.out.print("*");
+          else
+            System.out.print(" ");
+        }
+        System.out.println();
+      }
+    }
+}
